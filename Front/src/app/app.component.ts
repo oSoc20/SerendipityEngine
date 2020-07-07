4a64e311833a9674557be2f248155ce5d5adac45
Kasper Zutterman <kasper.zutterman@ugent.be>
2020-07-07 16:52:41 +0200

    add search mechanic

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -7,6 +7,7 @@ import Icon from 'ol/style/Icon';
 import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
 import TileLayer from 'ol/layer/Tile';
+import { MapboxService, Feature} from './services/mapbox/mapbox.service'
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,27 @@ export class AppComponent {
   title = 'SerendipityFront';
 
   map : Map;
-  constructor() {}
+  cities: string[] = [];
+  selectedCity = null;
 
+  constructor(private mapboxService: MapboxService) {}
+
+  search(event: any) {
+    const searchTerm = event.target.value.toLowerCase();
+    if (searchTerm && searchTerm.length > 0) {
+      this.mapboxService
+        .search_word(searchTerm)
+        .subscribe((features: Feature[]) => {
+          this.cities = features.map(feat => feat.place_name);
+        });
+    }
+    else {
+      this.cities = [];
+    }
+  }
+
+  onSelect(address: string) {
+    this.selectedCity = address;
+    this.cities = [];
+  }
 }
--- Front/src/environments/environment.ts
+++ Front/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiaGRlbHZhIiwiYSI6ImNrY2MxZmF2bzAwY3oycGp6aDYzZXZrNGYifQ.Va0SpldH0PEHv8FuThOrtg'
+  }
 };
 
 /*
