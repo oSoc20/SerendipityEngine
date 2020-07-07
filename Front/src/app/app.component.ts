98fee70949920bc061684617ff90d61e78a1e236
dormalexis <47395991+dormalexis@users.noreply.github.com>
2020-07-07 13:25:29 +0200

    Initial project

---
+++ Front/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SerendipityFront';
+
+  map : Map;
+  constructor() {}
+
+}
