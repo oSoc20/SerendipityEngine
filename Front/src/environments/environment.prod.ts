4b2888b291e5b344219278fecb4c5ceb89ce8ee3
Kasper Zutterman <kasper.zutterman@ugent.be>
2020-07-08 13:54:29 +0200

    fixed api call

--- Front/src/environments/environment.prod.ts
+++ Front/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiaGRlbHZhIiwiYSI6ImNrY2MxZmF2bzAwY3oycGp6aDYzZXZrNGYifQ.Va0SpldH0PEHv8FuThOrtg'
+  }
 };
