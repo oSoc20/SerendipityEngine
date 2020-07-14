65528642ddea7d99ac71f006f9f115eab13abdd1
Kasper Zutterman <kasper.zutterman@ugent.be>
2020-07-14 13:51:59 +0200

    Added tiles-in-bbox import

--- Front/src/app/services/opoi/opoi.service.ts
+++ Front/src/app/services/opoi/opoi.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { StoreService } from '../store/store.service';
+import * as t from "../../../../node_modules/tiles-in-bbox/index.js";
+//var t = require('tiles-in-bbox')
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class OpoiService {
 
-  constructor() { }
+  // bbox = {
+  //   bottom : 42.356,
+  //   left : -71.1279,
+  //   top : 42.3876,
+  //   right : -71.1002
+  // };
+  zoom = 15;
+  tiles;
+
+  constructor(public store : StoreService) { }
+
+  //Follows the GeoJson/OpenStreetMaps convention of a clockwise box starting at the bottom.
+  calculateTiles() {
+    this.tiles = t.tilesInBbox(this.store.selectedDestinationCity.bbox, this.zoom);
+    console.log(this.tiles);
+  }
+
 }
