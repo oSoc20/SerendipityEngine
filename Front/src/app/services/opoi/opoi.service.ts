4c4231bea95846023ab0e81ce29c40aab4591810
Kasper Zutterman <kasper.zutterman@ugent.be>
2020-07-14 15:18:35 +0200

    added tile calculation

--- Front/src/app/services/opoi/opoi.service.ts
+++ Front/src/app/services/opoi/opoi.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { StoreService } from '../store/store.service';
-import * as t from "../../../../node_modules/tiles-in-bbox/index.js";
+import * as t from "../../../../node_modules/tiles-in-bbox";
 //var t = require('tiles-in-bbox')
  
 @Injectable({
@@ -8,21 +8,21 @@ import * as t from "../../../../node_modules/tiles-in-bbox/index.js";
 })
 export class OpoiService {
 
-  // bbox = {
-  //   bottom : 42.356,
-  //   left : -71.1279,
-  //   top : 42.3876,
-  //   right : -71.1002
-  // };
-  zoom = 15;
-  tiles;
+  bbox = {
+    bottom: 4.313869,
+    left: 50.796245,
+    right: 50.913716,
+    top: 4.437046
+  };
+  zoom = 14;
+  tiles: [];
 
   constructor(public store : StoreService) { }
 
   //Follows the GeoJson/OpenStreetMaps convention of a clockwise box starting at the bottom.
   calculateTiles() {
-    this.tiles = t.tilesInBbox(this.store.selectedDestinationCity.bbox, this.zoom);
-    console.log(this.tiles);
+    this.tiles = t.tilesInBbox(this.getBbox(), this.zoom);
+    console.log("tiles:", this.tiles);
   }
 
   /*
@@ -33,6 +33,17 @@ export class OpoiService {
   */
   getUrl(long, lat, zoom) {
     return `https://opoi.org/${zoom}/${long}/${lat}`;
+  }
+
+  getBbox() {
+    var bbox = this.store.selectedDestinationCity.bbox;
+
+    return {
+      bottom: bbox[0],
+      left: bbox[1],
+      top : bbox[2],
+      right: bbox[3]
+    };
   }
 
 }
